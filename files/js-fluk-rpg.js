34a85746b6143deb7d6de0088619053d2306e8ac
yuroweb55 <168056659+yuroweb55@users.noreply.github.com>
2024-10-25 07:16:18 +0700

    Update js-fluk-rpg.js

--- files/js-fluk-rpg.js
+++ files/js-fluk-rpg.js
@@ -53,36 +53,6 @@ const data = {
         href: "mailto:thanyathon0202@gmail.com",
         icon: "Gmail",
         textKey: "text_6"
-    }, ]
+    } ]
 };
-document.head.appendChild(Object.assign(document.createElement("style"), {
-    type: "text/css",
-    innerText: `.cta{position:fixed;width:100%;height:100%;top:0;display:flex;justify-content:center;padding-top:40px;overflow:auto}a,a:hover{text-decoration:none;color:#000}.back{min-width:calc(100vw - 20px);min-height:100vh;background:url(https://yuroweb55.github.io/files/icon-fluk-rpg5.webp) no-repeat;background-position:center;background-size:cover;opacity:.125}main{text-align:center}.icon{width:150px;height:150px;border-radius:50%;margin-bottom:20px}.hhgs{max-width:500px;height:75px;background-color:#fff;box-shadow:10px 12px 20px rgba(0,0,0,.7);transition:all .1s ease 0s;border-radius:20px;display:flex;justify-content:space-between;align-items:center;padding:2.5px 20px;margin-bottom:25px}.hhgs:hover,#corner-button:hover{transform:scale(1.1)}.iconvb{background-size:cover;background-position:center;width:70px;height:70px}#corner-button{position:fixed;top:10px;right:10px;background-color:#007bff;color:#fff;padding:10px 10px;border-radius:50px;text-align:center;text-decoration:none;font-weight:700;font-size:14px;box-shadow:5px 6px 8px rgba(0,0,0,.7);transition:all .1s ease 0s}.pd0_5{padding:0 15px}`
-}));
-let lg = 'en';
-const content = document.getElementById('content')
-  , cornerButton = document.getElementById('corner-button');
-function renderContent() {
-    content.innerHTML = `<div><img loading="lazy" class="icon" src="https://yuroweb55.github.io/files/icon-fluk-rpg5.webp" alt></div><div><h3>fluk_rpg5</h3></div><div style="padding-top:50px"></div><div><h5 id="text-all">${data[lg].text_all}</h5></div>${data.links.map(l => `<div><a class="hhgs" ${l.href ? 'href="' + l.href + '"' : "onclick=\"copy('" + l.copy + "')\""}target="_blank"><div class="pd0_5"><div class="iconvb" style="background-image: url('https://fkwapp.com/09472/icon/${l.icon}.webp')"></div></div><div class="pd0_5"><b id="${l.textKey}">${data[lg][l.textKey]}</b></div><div class="pd0_5"></div></a></div>`).join('')}<div style="padding-top:60px"></div>`
-}
-;cornerButton.addEventListener('click', () => {
-    lg = lg === 'en' ? 'th' : 'en';
-    renderContent();
-    cornerButton.innerHTML = data[lg].btnText
-}
-);
-function copy(t) {
-    const textarea = document.createElement('textarea');
-    textarea.value = t;
-    document.body.appendChild(textarea);
-    textarea.select();
-    try {
-        document.execCommand('copy');
-        swal(data[lg].success, "", "success")
-    } catch (err) {
-        swal(data[lg].error, "", "error")
-    }
-    document.body.removeChild(textarea)
-}
-renderContent();
-cornerButton.innerHTML = data[lg].btnText
+document.head.appendChild(Object.assign(document.createElement("style"),{type:"text/css",innerText:`.cta{position:fixed;width:100%;height:100%;top:0;display:flex;justify-content:center;padding-top:40px;overflow:auto}a,a:hover{text-decoration:none;color:#000}.back{min-width:calc(100vw - 20px);min-height:100vh;background:url(https://yuroweb55.github.io/files/icon-fluk-rpg5.webp) no-repeat;background-position:center;background-size:cover;opacity:.125}main{text-align:center}.icon{width:150px;height:150px;border-radius:50%;margin-bottom:20px}.hhgs{max-width:500px;height:75px;background-color:#fff;box-shadow:10px 12px 20px rgba(0,0,0,.7);transition:all .1s ease 0s;border-radius:20px;display:flex;justify-content:space-between;align-items:center;padding:2.5px 20px;margin-bottom:25px}.hhgs:hover,#corner-button:hover{transform:scale(1.1)}.iconvb{background-size:cover;background-position:center;width:70px;height:70px}#corner-button{position:fixed;top:10px;right:10px;background-color:#007bff;color:#fff;padding:10px 10px;border-radius:50px;text-align:center;text-decoration:none;font-weight:700;font-size:14px;box-shadow:5px 6px 8px rgba(0,0,0,.7);transition:all .1s ease 0s}.pd0_5{padding:0 15px}`}));let lg='en';const content=document.getElementById('content'),cornerButton=document.getElementById('corner-button');function renderContent(){content.innerHTML=`<div><img loading="lazy" class="icon" src="https://yuroweb55.github.io/files/icon-fluk-rpg5.webp" alt></div><div><h3>fluk_rpg5</h3></div><div style="padding-top:50px"></div><div><h5 id="text-all">${data[lg].text_all}</h5></div>${data.links.map(l => `<div><a class="hhgs" ${l.href?'href="'+l.href+'"':"onclick=\"copy('"+l.copy+"')\""}target="_blank"><div class="pd0_5"><div class="iconvb" style="background-image: url('https://yuroweb55.github.io/files/icon/${l.icon}.webp')"></div></div><div class="pd0_5"><b id="${l.textKey}">${data[lg][l.textKey]}</b></div><div class="pd0_5"></div></a></div>`).join('')}<div style="padding-top:60px"></div>`};cornerButton.addEventListener('click',()=>{lg=lg==='en'?'th':'en';renderContent();cornerButton.innerHTML=data[lg].btnText});function copy(t){const textarea=document.createElement('textarea');textarea.value=t;document.body.appendChild(textarea);textarea.select();try{document.execCommand('copy');swal(data[lg].success,"","success")}catch(err){swal(data[lg].error,"","error")} document.body.removeChild(textarea)} renderContent();cornerButton.innerHTML=data[lg].btnText
